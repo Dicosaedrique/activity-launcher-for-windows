bb736075425818c46c6051bec629afc3a22bb065
Dicosaedrique <abouaban2@gmail.com>
2023-07-17 17:18:37 -0400

    add application controller

---
+++ Solution/FastWorkspace.Client/Common/Controllers/ApplicationController.cs
@@ -0,0 +1,54 @@
+﻿using FastWorkspace.Client.Common.Events;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using MudBlazor;
+
+namespace FastWorkspace.Client.Common.Controllers;
+
+public class ApplicationController
+{
+    protected readonly ApplicationEventManager _eventManager;
+
+    protected readonly ISnackbar _notificationService;
+
+    protected readonly IDialogService _dialogService;
+
+    protected readonly IStringLocalizer<AppLocales> _localize;
+
+    protected readonly ILogger<ApplicationController> _logger;
+
+    public ApplicationController(ApplicationEventManager eventManager, ISnackbar notificationService, IDialogService dialogService, IStringLocalizer<AppLocales> localize, ILogger<ApplicationController> logger)
+    {
+        _eventManager = eventManager;
+        _notificationService = notificationService;
+        _dialogService = dialogService;
+        _localize = localize;
+        _logger = logger;
+    }
+
+    protected async Task PublishEvent(ApplicationEventType eventType, object? data = null)
+    {
+        await _eventManager.PublishEvent(this, eventType, data);
+    }
+
+    protected async Task PublishError(ErrorEventDetails details)
+    {
+        // todo: ajouter une gestion des erreurs par snackbar
+        await _eventManager.PublishError(this, details);
+    }
+
+    public void Notify(string title, Severity severity = Severity.Normal)
+    {
+        _notificationService.Add(title, severity);
+    }
+
+    public void NotifySuccess(string title)
+    {
+        Notify(title, Severity.Success);
+    }
+
+    public void NotifyError(string title)
+    {
+        Notify(title, Severity.Error);
+    }
+}
